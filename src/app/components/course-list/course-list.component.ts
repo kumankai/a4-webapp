b141a307193db4ad84f6a8e87d0a18791438d329
Jacob Angga <anggaj@sheridancollege.ca>
2024-04-11 19:55:21 -0400

    Course list and course edit

---
+++ src/app/components/course-list/course-list.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import {NgForOf} from "@angular/common";
+import {CourseService} from "../../services/course.service";
+import {CourseModel} from "../../models/course.model";
+
+@Component({
+  selector: 'app-course-list',
+  standalone: true,
+  imports: [RouterLink, NgForOf, RouterLinkActive],
+  templateUrl: './course-list.component.html',
+  styleUrl: './course-list.component.css'
+})
+export class CourseListComponent {
+  courseList?:CourseModel[];
+
+  constructor(private courseService:CourseService) {
+  }
+
+  ngOnInit(): void {
+    this.getCourseList();
+  }
+
+  getCourseList(){
+    this.courseService.getAllCourses()
+      .subscribe({
+        next:(data)=>{
+          this.courseList = data;
+          console.log(data);
+        },
+        error:(e)=>console.error(e)
+      });
+  }
+}
--- src/app/components/edit-course/edit-course.component.ts
+++ src/app/components/edit-course/edit-course.component.ts
@@ -17,8 +17,31 @@ export class EditCourseComponent implements OnInit{
 
   constructor(private courseService:CourseService, private route: ActivatedRoute) {
   }
+
+  ngOnInit(): void {
+    this.getCourse(this.route.snapshot.params['id']);
+    console.log("Student id = " + this.course?.id);
+    console.log("Student name = " + this.course?.coursename);
+  }
+
+  getCourse(id:any) {
+    this.courseService.getCourseById(id)
+      .subscribe({
+        next:(data)=>{
+          this.course = data;
+          console.log(data);
+        },
+        error:(e)=>console.error(e)});
+  }
+
   updateCourse() {
     if (this.course?.id) {
+      this.courseService.updateCourse(this.course, this.course?.id).subscribe(
+        (data) => {
+          console.log(data);
+        }
+      );
+      console.log("Update course " + this.course?.coursename + " id = " + this.course?.id);
     }
   }
 }
